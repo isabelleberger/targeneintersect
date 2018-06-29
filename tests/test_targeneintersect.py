f10e7d918f4aa24081c0a9f1ce9ab3976fec390f
isabelleberger <bergeric@nih.gov>
2018-06-29 16:45:43 -0400

    removes cli import from test.py

--- tests/test_targeneintersect.py
+++ tests/test_targeneintersect.py
@@ -5,10 +5,7 @@
 
 import pytest
 
-from click.testing import CliRunner
-
 from targeneintersect import targeneintersect
-from targeneintersect import cli
 
 
 @pytest.fixture
@@ -25,17 +22,6 @@ def test_content(response):
     """Sample pytest test function with the pytest fixture as an argument."""
     # from bs4 import BeautifulSoup
     # assert 'GitHub' in BeautifulSoup(response.content).title.string
-
-
-def test_command_line_interface():
-    """Test the CLI."""
-    runner = CliRunner()
-    result = runner.invoke(cli.main)
-    assert result.exit_code == 0
-    assert 'targeneintersect.cli.main' in result.output
-    help_result = runner.invoke(cli.main, ['--help'])
-    assert help_result.exit_code == 0
-    assert '--help  Show this message and exit.' in help_result.output
 
 #def test_target_gene_intersect(capsys):
     #assert targeneintersect.target_gene_intersect(
